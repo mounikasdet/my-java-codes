80a9a31f1c24723bb73185a434932f9aa94532af
mounikasdet <mounikateegala77@gmail.com>
2026-09-10 08:32:44 -0700

    Implement number swapping in P3_SwapNumbers

--- P3_SwapNumbers.java
+++ P3_SwapNumbers.java
@@ -1,1 +1,12 @@
 
+public class P3_SwapNumbers {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("Before: a=" + a + ", b=" + b);
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("After: a=" + a + ", b=" + b);
+    }
+}
