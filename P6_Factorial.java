697e2c1873d4e7553c60250f4d948a1594beb965
mounikasdet <mounikateegala77@gmail.com>
2026-09-10 08:43:13 -0700

    Implement factorial calculation in P6_Factorial.java

---
+++ P6_Factorial.java
@@ -0,0 +1,14 @@
+public class P6_Factorial {
+    public static void main(String[] args) {
+        int num = 5;
+        long factorial = 1;
+        
+        for(int i=1; i<=num; i++) {
+            factorial = factorial * i;
+        }
+        
+        System.out.println("Factorial of " + num + " is: " + factorial);
+        
+        
+    }
+}
